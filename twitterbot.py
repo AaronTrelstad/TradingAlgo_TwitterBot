2a02bfb85b05d01dc6d744f634d8528339fbe0ce
Aaron Trelstad <108637601+AaronTrelstad@users.noreply.github.com>
2022-12-12 22:15:11 -0600

    Update twitterbot.py

--- twitterbot.py
+++ twitterbot.py
@@ -20,34 +20,98 @@ def buysell():
     high52 = stock.info['fiftyTwoWeekHigh']
     pricerange = [low52 * 0.1 + low * 0.9, low, high, high52 * 0.1 + high * 0.9]
     stand = stdev(pricerange)
-    buy = ma - stand
-    sell = ma + stand
     iowaTz = pytz.timezone("US/Central")
     timeInIowa = datetime.now(iowaTz)
     currentTime = timeInIowa.strftime("%H:%M:%S")
 
-    if price <= buy:
-        return(f"Buy Signal detected for {tick} at price of {price}. ({currentTime} CST)")
-
-    if price >= sell:
-        return(f"Sell Signal detected for {tick} at price of {price}. ({currentTime} CST)")
-
+    #rsi
+    closesr = stock.history(period='2wk', interval='1d')
+    closer = []
+    dayr = []
+    neg = 0
+    pos = 0
+    for i in range(0, len(closesr["Close"])):
+        closer.append(closesr["Close"][i])
+        dayr.append(i)
+    closer = np.array(closer)
+    dayr = np.array(dayr)
+    for x in range(1, len(closer)):
+        change = (closer[x] - closer[x - 1]) / closer[x]
+        if change < 0:
+            neg += change
+        elif change > 0:
+            pos += change
+    pos = (pos / 14)
+    neg = (neg / 14) * -1
+    rsi = 100 - (100 / (1 + (pos / neg)))
+    if rsi < 40:
+        rsi = (f"RSI indicates that {tick} is oversold.")
+    elif rsi > 60:
+        rsi = (f"RSI indicates that {tick} is overbought.")
     else:
-        closes = stock.history(period='1wk', interval='1d')
-        close = []
-        day = []
-        for i in range(0, len(closes["Close"])):
-            close.append(closes["Close"][i])
-            day.append(i)
-        close = np.array(close)
-        day = np.array(day).reshape((-1, 1))
-        model = LinearRegression().fit(day, close)
-        intc = model.intercept_
-        coef = model.coef_
-        if model.coef_ > 0:
-            return(f"Neutral Signal detected. Current linear regression model predicts a sell signal in {(sell - intc) / coef} trading days. ({currentTime} CST)")
-        if model.coef_ < 0:
-            return(f"Neutral Signal detected. Current linear regression model predicts a buy signal in {(buy - intc) / coef} trading days. ({currentTime} CST)")
+        rsi = (f"RSI gives neutral signal for {tick}.")
+
+    ##Wk lin
+    closeswk = stock.history(period='1wk', interval='1d')
+    closewk = []
+    daywk = []
+    for i in range(0, len(closeswk["Close"])):
+        closewk.append(closeswk["Close"][i])
+        daywk.append(i)
+    closewk = np.array(closewk)
+    daywk = np.array(daywk).reshape((-1, 1))
+    modelwk = LinearRegression().fit(daywk, closewk)
+    intcwk = modelwk.intercept_
+    coefwk = modelwk.coef_
+
+    ##mo lin
+    closesmo = stock.history(period='1mo', interval='1d')
+    closemo = []
+    daymo = []
+    for i in range(0, len(closesmo["Close"])):
+        closemo.append(closesmo["Close"][i])
+        daymo.append(i)
+    closemo = np.array(closemo)
+    daymo = np.array(daymo).reshape((-1, 1))
+    modelmo = LinearRegression().fit(daymo, closemo)
+    intcmo = modelmo.intercept_
+    coefmo = modelmo.coef_
+    ##3m
+    closes3mo = stock.history(period='3mo', interval='1d')
+    close3mo = []
+    day3mo = []
+    for i in range(0, len(closes3mo["Close"])):
+        close3mo.append(closes3mo["Close"][i])
+        day3mo.append(i)
+    close3mo = np.array(close3mo)
+    day3mo = np.array(day3mo).reshape((-1, 1))
+    model3mo = LinearRegression().fit(day3mo, close3mo)
+    intc3mo = model3mo.intercept_
+    coef3mo = model3mo.coef_
+
+    ##Situations
+    if coef3mo < 0 and coefmo > 0 and coefwk > coefmo:
+        return((f"Linear reggression model predicts BUY signal for {tick} at price of {price}. {rsi} ({currentTime} CST)"))
+    if coef3mo > 0 and coefmo > 0 and coefwk > coefmo:
+        return((f"Linear reggression model predicts BUY signal for {tick} at price of {price}. {rsi} ({currentTime} CST)"))
+    if coef3mo < 0 and coefmo < 0 and coefmo > coef3mo and coefwk > coefmo and coefwk > 0:
+        return((f"Linear reggression model predicts BUY signal for {tick} at price of {price}. {rsi} ({currentTime} CST)"))
+    if coef3mo > 0 and coefmo < 0 and coefwk > 0:
+        return((f"Linear reggression model predicts BUY signal for {tick} at price of {price}. {rsi} ({currentTime} CST)"))
+    if coef3mo > 0 and coefmo > coef3mo and coefwk > coefmo:
+        return((f"Linear reggression model predicts BUY signal for {tick} at price of {price}. {rsi} ({currentTime} CST)"))
+    if coef3mo < 0 and coefmo < coef3mo and coefwk < coefmo:
+        return((f"Linear reggression model predicts SELL signal for {tick} at price of {price}. {rsi} ({currentTime} CST)"))
+    if coef3mo < 0 and coefmo > 0 and coefwk < 0:
+        return((f"Linear reggression model predicts SELL signal for {tick} at price of {price}. {rsi} ({currentTime} CST)"))
+    if coef3mo > 0 and coef3mo > coefmo and coefwk < 0:
+        return((f"Linear reggression model predicts SELL signal for {tick} at price of {price}. {rsi} ({currentTime} CST)"))
+    if coef3mo > 0 and coefmo < 0 and coefwk < coefmo:
+        return((f"Linear reggression model predicts SELL signal for {tick} at price of {price}. {rsi} ({currentTime} CST)"))
+    if coef3mo < 0 and coefmo < 0 and coefwk < coefmo:
+        return((f"Linear reggression model predicts SELL signal for {tick} at price of {price}. {rsi} ({currentTime} CST)"))
+    else:
+        return((f"Neutral signal detected for {tick} at price of {price}. {rsi} ({currentTime} CST)"))
 
 
 def api():
@@ -55,11 +119,8 @@ def api():
     auth.set_access_token(keys.access_token, keys.access_token_secret)
     return(tweepy.API(auth))
 
-def tweet(api: tweepy.API, message: str, image_path=None):
-    if image_path:
-        api.update_status_with_media(message, image_path)
-    else:
-        api.update_status(message)
+def tweet(api: tweepy.API, message: str):
+    api.update_status(message)
     print('Tweeted')
 
 if __name__ == '__main__':
